6e635b1df1487b175ef7f854479f2320fe9e3b59
Dreambig12 <sciencefi2@gmail.com>
2022-04-15 19:36:06 +0200

    setting up the environment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes =  [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+
+      // {
+      //   path: 'user',
+      //   loadChildren: () => import('src/app/modules/user/user.module').then(m => m.UserModule)
+      // },
+      // { path: 'login', component: LoginComponent },
+      // { path: 'login/:error', component: LoginComponent },
+      // { path: 'register', component: RegisterComponent },
+      // { path: 'register/:error', component: RegisterComponent },
+      // { path: 'product/:ID', component: ProductDetailsComponent },
+
+    ]
+  },
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
