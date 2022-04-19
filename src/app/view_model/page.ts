6a72cd63ba875b5cdf7534411a084adb39bead93
gohar <Omar.mostafa2010@science.bsu.edu.eg>
2022-04-19 16:33:17 +0200

    data table

---
+++ src/app/view_model/page.ts
@@ -0,0 +1,9 @@
+export interface Page {
+    size: number ;
+    // The total number of elements
+    totalElements: number ;
+    // The total number of pages
+    totalPages: number ;
+    // The current page number
+    pageNumber: number;
+}
